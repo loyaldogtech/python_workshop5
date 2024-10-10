7e16682c760f1b1287c2758a6ee88d510e92af56
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-10 12:13:02 -0700

    Task 2 complete

--- workshop5.py
+++ workshop5.py
@@ -15,5 +15,21 @@ def guess_random_number(tries, start, stop):
         tries -= 1
     print("Sorry, you ran out of tries. The number was", number_to_guess)
     
+def guess_random_number_linear(tries, start, stop):
+    number_to_guess = random.randint(start, stop)
+    print(f"The number to guess is: {number_to_guess}")
+    for i in range(start, stop + 1):
+        tries -= 1
+        print(f"Tries remaining: {tries}")
+        print(f"Guess the number: {i}")
+        if tries == 0:
+            print("Sorry, you ran out of tries. The number was", number_to_guess)
+            return
+        if i == number_to_guess:
+            print("You guessed it!")
+            return
 
-guess_random_number(5, 1, 100)
+
+guess_random_number_linear(5, 0, 10)
+
+# guess_random_number(5, 1, 100)
