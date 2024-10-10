73aaa95b0ab980fda4bd2e5b3792ee4af612560c
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-10 12:29:27 -0700

    Task 3 completed

--- workshop5.py
+++ workshop5.py
@@ -14,7 +14,8 @@ def guess_random_number(tries, start, stop):
             return
         tries -= 1
     print("Sorry, you ran out of tries. The number was", number_to_guess)
-    
+
+
 def guess_random_number_linear(tries, start, stop):
     number_to_guess = random.randint(start, stop)
     print(f"The number to guess is: {number_to_guess}")
@@ -27,9 +28,34 @@ def guess_random_number_linear(tries, start, stop):
             return
         if i == number_to_guess:
             print("You guessed it!")
+
+def guess_random_num_binary(tries, start, stop):
+    number_to_guess = random.randint(start, stop)
+    print(f"Random number to guess is: {number_to_guess}")
+    low = start
+    high = stop
+    while tries != 0:
+        print(f"Tries remaining: {tries}")
+        guess = (low + high) // 2
+        print(f"Guess the number: {guess}")
+        if guess < number_to_guess:
+            print("Guess Higher!")
+            low = guess + 1
+        elif guess > number_to_guess:
+            print("Guess Lower!")
+            high = guess - 1
+        else:
+            print("You guessed it!")
             return
+        tries -= 1
+    print("Sorry, you ran out of tries. The number was", number_to_guess)
 
 
-guess_random_number_linear(5, 0, 10)
+
+
+
+guess_random_num_binary(5, 0, 100)
+
+#guess_random_number_linear(5, 0, 10)
 
 # guess_random_number(5, 1, 100)
