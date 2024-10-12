78cb333d999ccd9c63d5b609950d0636b1d2cf6c
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-12 12:58:57 -0700

    Bonus 1 & 2 completed

--- workshop5_copy.py
+++ workshop5_copy.py
@@ -2,10 +2,11 @@ import random
 
 def guess_random_number(tries, start, stop):
     number_to_guess = random.randint(start, stop)
-    print(f"The random number to guess is: {number_to_guess}")
+    print(f"The nu")
     while tries != 0:
         print(f"Tries remaining: {tries}")
 
+        # Input validation to ensure the guess is an integer within the given range
         while True:
             try:
                 guess = int(input(f"Guess a number between {start} and {stop}: "))
@@ -31,7 +32,7 @@ def guess_random_number(tries, start, stop):
 
 def guess_random_number_linear(tries, start, stop):
     number_to_guess = random.randint(start, stop)
-    print(f"The random number to guess is: {number_to_guess}")
+    print(f"The number to guess is: {number_to_guess}")
     for i in range(start, stop + 1):
         tries -= 1
         print(f"Tries remaining: {tries}")
@@ -44,7 +45,7 @@ def guess_random_number_linear(tries, start, stop):
 
 def guess_random_num_binary(tries, start, stop):
     number_to_guess = random.randint(start, stop)
-    print(f"The random number to guess is: {number_to_guess}")
+    print(f"Random number to guess is: {number_to_guess}")
     low = start
     high = stop
     while tries != 0:
@@ -63,46 +64,12 @@ def guess_random_num_binary(tries, start, stop):
         tries -= 1
     print("Sorry, you ran out of tries. The number was", number_to_guess)
 
-def play_guessing_game():
-    while True:
-        try:
-            tries = int(input("Enter the number of tries: "))
-            start = int(input("Enter the start of the range: "))
-            stop = int(input("Enter the end of the range: "))
-            if start < stop:
-                break
-            else:
-                print("The start of the range must be less than the stop.")
-        except ValueError:
-            print("Please enter valid integers for tries, start, and stop.")
-
-    print("\nChoose the guessing method:")
-    print("1. Manual guess")
-    print("2. Linear search")
-    print("3. Binary search")
-
-    while True:
-        try:
-            choice = int(input("Enter your choice (1, 2, or 3): "))
-            if choice in [1, 2, 3]:
-                break
-            else:
-                print("Please enter 1, 2, or 3.")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-    if choice == 1:
-        guess_random_number(tries, start, stop)
-    elif choice == 2:
-        guess_random_number_linear(tries, start, stop)
-    elif choice == 3:
-        guess_random_num_binary(tries, start, stop)
 
 
-# guess_random_num_binary(5, 0, 100)
 
-# guess_random_number_linear(5, 0, 10)
 
-#guess_random_number(5, 1, 100)
+#guess_random_num_binary(5, 0, 100)
 
-play_guessing_game()
+#guess_random_number_linear(5, 0, 10)
+
+guess_random_number(5, 1, 100)
